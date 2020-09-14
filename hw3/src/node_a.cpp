49e30fc702ffa1cfb0852e3d11d3dd581252f2df
Raschild6 <michele.thiella@studenti.unipd.it>
2020-09-14 16:32:51 +0200

    Auto stash before merge of "hw3" and "origin/hw3"

--- hw3/src/node_a.cpp
+++ hw3/src/node_a.cpp
@@ -1,15 +1,5 @@
-#include <ros/ros.h>
-#include <move_base_msgs/MoveBaseAction.h>
-#include <actionlib/client/simple_action_client.h>
+#include "node_a.h"
 
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_listener.h>
-#include <tf2_eigen/tf2_eigen.h>
-//#include <tf/transform_broadcaster.h>
-#include <tf/transform_datatypes.h>
-
-#include <tf2/LinearMath/Quaternion.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
