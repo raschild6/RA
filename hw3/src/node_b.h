c9f9323f64225b73d38b7759d27d2a7aad60ecb1
Raschild6 <michele.thiella@studenti.unipd.it>
2020-09-15 16:04:30 +0200

    node_b.h added

---
+++ hw3/src/node_b.h
@@ -0,0 +1,18 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_datatypes.h>
+
+#include <tf2/LinearMath/Quaternion.h>
+
+#include <costmap_2d/costmap_2d_ros.h>
+#include <dwa_local_planner/dwa_planner_ros.h>
+
