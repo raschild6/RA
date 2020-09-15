c27a240c00119f7db63ab60c4a46b6b40ac5b8f4
Raschild6 <michele.thiella@studenti.unipd.it>
2020-09-15 16:03:25 +0200

    fix transform odom to map

--- hw3/src/node_a.cpp
+++ hw3/src/node_a.cpp
@@ -6,17 +6,19 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "transform_broadcast_marrtino");
   ros::NodeHandle n;
-  ros::Rate rate(1000);
-  ros::Publisher odom_map_tf = n.advertise<tf2_msgs::TFMessage>("marrtino/amcl/tf", 1);
+  ros::Rate rate(100);
+  ros::Publisher odom_map_tf = n.advertise<tf2_msgs::TFMessage>("/tf", 100);
 
   tf2_msgs::TFMessage tf_message;
+  
 
   tf2_ros::Buffer tfBuffer;
   tf2_ros::TransformListener tfListener(tfBuffer);
   geometry_msgs::TransformStamped transformStamped;
 
-  ros::Duration timeout(0.1);
+  ros::Duration timeout(3.0);
 
+  ros::Duration(1.0).sleep();
   while (ros::ok())
   {
     try
@@ -31,14 +33,19 @@ int main(int argc, char **argv)
       ROS_INFO("Error Trasformation...%s", ex.what());
     }
 
+    transformStamped.header.frame_id = "marrtino_odom";
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.child_frame_id = "marrtino_map";
     tf_message.transforms = {transformStamped};
-    //tf_message.transforms[0].header.frame_id = "marrtino_odom";
-    //tf_message.transforms[0].child_frame_id = "marrtino_map";
+
     //tf::quaternionTFToMsg(tf::Quaternion(0.015, 0.000, 0.005, 1.000), tf_message.transforms[0].transform.rotation);
     //tf::vector3TFToMsg(tf::Vector3(-0.135, -0.044, -0.028), tf_message.transforms[0].transform.translation);
+    
     odom_map_tf.publish(tf_message);
+    
     ros::spinOnce();
-
+    rate.sleep();
   }
+  
   return 0;
 }
