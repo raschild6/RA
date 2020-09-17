15a2079e38c398a35b5e7cc0de5e372ef4d77b75
Raschild6 <michele.thiella@studenti.unipd.it>
2020-09-17 09:57:54 +0200

    relative goal pose and feedback status goal implemented

--- hw3/src/node_b.cpp
+++ hw3/src/node_b.cpp
@@ -1,77 +1,189 @@
-#include "node_a.h"
+#include "node_b.h"
+
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "simple_navigation_goals");
-  ros::NodeHandle n;
-  //ros::AsyncSpinner spinner(0); // Allow to get messages in async way (thread). 0 means: use all processor of machine
-  ros::Publisher odom_map_tf = n.advertise<tf2_msgs::TFMessage>("marrtino/amcl/tf", 1);
-  //spinner.start();
+geometry_msgs::Pose current_amcl_pose;
+geometry_msgs::PoseStamped current_goal_map_pose;
+
+bool amcl_set = false;
+int goal_plan_status = -1;
 
 
-  //tell the action client that we want to spin a thread by default
+void sendMyGoal(geometry_msgs::PoseStamped target_pose){
   MoveBaseClient ac("marrtino/move_base", true);
-
-  //wait for the action server to come up
-  while (!ac.waitForServer(ros::Duration(5.0)))
-  {
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
-
   move_base_msgs::MoveBaseGoal goal;
-  geometry_msgs::PoseStamped target_pose;
-
-  //we'll send a goal to the robot to move 1 meter forward
-
-  target_pose.pose.position.x = 1.0;
-  target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(0);
-
+  
   tf2_ros::Buffer tfBuffer;
   tf2_ros::TransformListener tfListener(tfBuffer);
   geometry_msgs::TransformStamped transformStamped;
   geometry_msgs::PoseStamped target_pose_tf;
 
-  ros::Duration timeout(1);
+  ros::Duration timeout(5);
   try
   {
-    transformStamped = tfBuffer.lookupTransform("marrtino_map", "marrtino_base_footprint", ros::Time(0), timeout);
+    transformStamped = tfBuffer.lookupTransform("marrtino_map", target_pose.header.frame_id, ros::Time(0), timeout);
     tf2::doTransform(target_pose, target_pose_tf, transformStamped);
   }
   catch (tf2::TransformException &ex)
   {
     ROS_INFO("Error Trasformation...%s", ex.what());
   }
-
+  
+  goal.target_pose = target_pose_tf;
   goal.target_pose.header.frame_id = "marrtino_map";
   goal.target_pose.header.stamp = ros::Time::now();
-  goal.target_pose = target_pose_tf;
 
-  ROS_INFO("Sending goal");
+  current_goal_map_pose = target_pose_tf;
+  ROS_INFO("Goal pose (marrtino_map): [%f, %f, %f] - [%f, %f, %f, %f]", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y,
+      goal.target_pose.pose.position.z, goal.target_pose.pose.orientation.x, goal.target_pose.pose.orientation.y, 
+      goal.target_pose.pose.orientation.z, goal.target_pose.pose.orientation.w);
+  ROS_INFO(" ----- Sending goal -----");
+
   ac.sendGoal(goal);
 
-  /*
-  geometry_msgs::PoseWithCovarianceStamped initial_pose;
-  initial_pose.pose.pose.position.x = -1.31;
-  initial_pose.pose.pose.position.y = 0;
-  initial_pose.pose.pose.position.z = 0;
-  tf2Scalar roll = 0;
-  tf2Scalar pitch = 0;
-  tf2Scalar yaw = 1.57;
+}
 
-  tf2::Quaternion rpy;
-  rpy.setEuler(yaw, pitch, roll);
-  tf2::convert(initial_pose.pose.pose.orientation, rpy);
-  */
+void resultFeedback(const move_base_msgs::MoveBaseActionFeedback::ConstPtr &feedback_msgs){
 
-  ac.waitForResult();
+  move_base_msgs::MoveBaseActionFeedback  feedback = *feedback_msgs;
+  goal_plan_status = feedback.status.status;
+}
 
-  if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1 meter forward");
-  else
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
+void correctPoseWRTOdom(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &amcl_pose_msgs){
   
-  //ros::waitForShutdown();
+  geometry_msgs::PoseWithCovarianceStamped amcl_pose_stamped = *amcl_pose_msgs;
+  geometry_msgs::Pose amcl_pose = amcl_pose_stamped.pose.pose;
+  
+  if(!amcl_set){
+    current_amcl_pose = amcl_pose;
+    amcl_set = true;
+    ROS_INFO("Current AMCL pose : [%f, %f, %f] - [%f, %f, %f, %f]", amcl_pose.position.x,
+        amcl_pose.position.y, amcl_pose.position.z,
+        amcl_pose.orientation.x, amcl_pose.orientation.y,
+        amcl_pose.orientation.z, amcl_pose.orientation.w); 
+    return;
+  }
+
+  ROS_INFO("New AMCL pose : [%f, %f, %f] - [%f, %f, %f, %f]", amcl_pose.position.x,
+        amcl_pose.position.y, amcl_pose.position.z,
+        amcl_pose.orientation.x, amcl_pose.orientation.y,
+        amcl_pose.orientation.z, amcl_pose.orientation.w); 
+
+  current_goal_map_pose.pose.position.x += amcl_pose.position.x - current_amcl_pose.position.x;
+  current_goal_map_pose.pose.position.y += amcl_pose.position.y - current_amcl_pose.position.y;
+  current_goal_map_pose.pose.position.z += amcl_pose.position.z - current_amcl_pose.position.z;
+  
+  tf2::Quaternion q_current_goal, q_old_odom, q_odom, q_translation;
+  tf2::fromMsg(current_goal_map_pose.pose.orientation, q_current_goal);
+  tf2::fromMsg(current_amcl_pose.orientation, q_old_odom);
+  tf2::fromMsg(amcl_pose.orientation, q_odom);
+  q_translation = q_odom * q_old_odom.inverse();
+  
+  ROS_INFO("Relative traslation values: [%f, %f, %f] - [%f, %f, %f, %f]", amcl_pose.position.x - current_amcl_pose.position.x, 
+            amcl_pose.position.y - current_amcl_pose.position.y, amcl_pose.position.z - current_amcl_pose.position.z, 
+            q_translation.getX(), q_translation.getY(), q_translation.getZ(), q_translation.getW());
+
+  current_goal_map_pose.pose.orientation = tf2::toMsg(q_translation * q_current_goal);
+  
+  current_amcl_pose.position = amcl_pose.position;  
+  current_amcl_pose.orientation = amcl_pose.orientation;
+
+  current_goal_map_pose.header.frame_id = "marrtino_map";
+  current_goal_map_pose.header.stamp = ros::Time::now();
+
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
+  geometry_msgs::TransformStamped transformStamped;
+  geometry_msgs::Pose target_pose_tf;
+  
+  ros::Duration timeout(10);
+  try{
+    transformStamped = tfBuffer.lookupTransform("marrtino_base_footprint", "marrtino_map", ros::Time(0), timeout);
+    tf2::doTransform(current_goal_map_pose.pose, target_pose_tf, transformStamped);
+  }
+  catch (tf2::TransformException &ex){
+    ROS_INFO("Error Trasformation...%s", ex.what());
+  }
+
+  current_goal_map_pose.pose = target_pose_tf;
+  current_goal_map_pose.header.frame_id = "marrtino_base_footprint";
+  current_goal_map_pose.header.stamp = ros::Time::now();
+
+  ROS_INFO("-> Goal pose update (marrtino_base_footprint): [%f, %f, %f] - [%f, %f, %f, %f]", current_goal_map_pose.pose.position.x, current_goal_map_pose.pose.position.y,
+      current_goal_map_pose.pose.position.z, current_goal_map_pose.pose.orientation.x, current_goal_map_pose.pose.orientation.y, 
+      current_goal_map_pose.pose.orientation.z, current_goal_map_pose.pose.orientation.w);
+
+  sendMyGoal(current_goal_map_pose);
+  
+}
+
+int main(int argc, char **argv){
+
+  ros::init(argc, argv, "simple_navigation_goals");
+  ros::NodeHandle n;
+  
+  ros::Rate r(50);
+  ros::AsyncSpinner spinner(0);
+  spinner.start();
+
+  //ros::Subscriber odometry_marrtino = n.subscribe("/marrtino/marrtino_base_controller/odom", 1, currentOdometry);
+  ros::Subscriber feedback = n.subscribe("/marrtino/move_base/feedback", 1, resultFeedback);
+  ros::Subscriber correct_goal_pose = n.subscribe("/marrtino/amcl_pose", 1, correctPoseWRTOdom);
+
+  current_goal_map_pose.header.frame_id = "marrtino_base_footprint";
+  current_goal_map_pose.header.stamp = ros::Time::now();
+  current_goal_map_pose.pose.position.x = 0.5;
+  current_goal_map_pose.pose.orientation = tf::createQuaternionMsgFromYaw(0);
+  
+  ROS_INFO("Fist Goal pose (marrtino_base_footprint): [%f, %f, %f] - [%f, %f, %f, %f]", current_goal_map_pose.pose.position.x, current_goal_map_pose.pose.position.y,
+      current_goal_map_pose.pose.position.z, current_goal_map_pose.pose.orientation.x, current_goal_map_pose.pose.orientation.y, 
+      current_goal_map_pose.pose.orientation.z, current_goal_map_pose.pose.orientation.w);
+
+  sendMyGoal(current_goal_map_pose);
+  
+  r.sleep();
+
+  /** Status code /
+    uint8 status
+    uint8 PENDING         = 0   # The goal has yet to be processed by the action server
+    uint8 ACTIVE          = 1   # The goal is currently being processed by the action server
+    uint8 PREEMPTED       = 2   # The goal received a cancel request after it started executing
+                                #   and has since completed its execution (Terminal State)
+    uint8 SUCCEEDED       = 3   # The goal was achieved successfully by the action server (Terminal State)
+    uint8 ABORTED         = 4   # The goal was aborted during execution by the action server due
+                                #    to some failure (Terminal State)
+    uint8 REJECTED        = 5   # The goal was rejected by the action server without being processed,
+                                #    because the goal was unattainable or invalid (Terminal State)
+    uint8 PREEMPTING      = 6   # The goal received a cancel request after it started executing
+                                #    and has not yet completed execution
+    uint8 RECALLING       = 7   # The goal received a cancel request before it started executing,
+                                #    but the action server has not yet confirmed that the goal is canceled
+    uint8 RECALLED        = 8   # The goal received a cancel request before it started executing
+                                #    and was successfully cancelled (Terminal State)
+    uint8 LOST            = 9   # An action client can determine that a goal is LOST. This should not be
+                                #    sent over the wire by an action server
+  /**/
+
+  while(ros::ok()){
+    if(goal_plan_status != -1 && goal_plan_status != 1){
+      ROS_INFO("Goal Status: %d", goal_plan_status);
+      r.sleep();
+    }
+    /*else if(goal_plan_status == 1){
+      r.sleep();
+    }else if(goal_plan_status == 3){
+      current_goal_map_pose.pose.position.y += 0.5;
+      if(current_goal_map_pose.pose.position.y < 2.7)
+        sendMyGoal(current_goal_map_pose);
+      else
+        return 0;
+    }else{
+      //ROS_INFO("Goal Status: %d", goal_plan_status);
+    }*/
+  }
+
+  ros::waitForShutdown();
+  
   return 0;
 }
--- hw3/src/node_b.h
+++ hw3/src/node_b.h
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <actionlib/server/simple_action_server.h>
 
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/buffer.h>
@@ -13,6 +14,15 @@
 
 #include <tf2/LinearMath/Quaternion.h>
 
-#include <costmap_2d/costmap_2d_ros.h>
-#include <dwa_local_planner/dwa_planner_ros.h>
+// Feedback
+//#include <move_base_msgs/MoveBaseActionFeedback.h>
 
+//Odometry
+//#include <nav_msgs/Odometry.h>
+
+// Wrapper of planner
+//#include <costmap_2d/costmap_2d_ros.h>
+//#include <dwa_local_planner/dwa_planner_ros.h>
+#include <nav_core/base_global_planner.h>
+#include <nav_core/base_local_planner.h>
+#include <nav_core/recovery_behavior.h>
